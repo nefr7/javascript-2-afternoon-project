0ae06c77a0bcdc8b485959c3efa416203e954a89
John Doe <mcp191@icloud.com>
2020-07-10 15:17:14 -0500

    Final

--- objects/practice.js
+++ objects/practice.js
@@ -14,7 +14,10 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: "Michael",
+  age: 18
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +26,30 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: "Porter Robinson",
+  food: "Fried Chicken",
+  person: "Chris",
+  book: "System Failure",
+  movie: "The Matrix",
+  holiday: "Thanksgiving or Christmas"
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Mercedes G-Wagon";
+favoriteThings.brand = "adidas";
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Fried Chicken";
+favoriteThings.book = "System Failure";
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +61,16 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+let backPack = {}
+let item = "pocket";
+backPack[item] = "headphones";
+backPack.color = "black";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+//alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +78,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +99,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in";
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
